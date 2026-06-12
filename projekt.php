ae4d612008c17f55c6cb043c39d987616bf6067c
SvenVucetic <svucetic@tvz.hr>
2026-06-12 12:37:19 +0200

    Add files via upload

---
+++ projekt.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-Biblija</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<header>
+    <div class="naslov">
+    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fbible%2Fbible_PNG4.png&f=1&nofb=1&ipt=305becaa2b033e8aa97d166366ce4dfd2a7b1fd39a9999d2271914421aeba069">
+    <h1>e-Bible</h1>
+</div>
+</header>
+<body>
+
+<div class="content">
+    <form action="" method="post">
+    <div class="dropdown">
+            <button class="dropbtn">
+                Book
+            </button>
+            <div class="dropdown-content">
+                <div class="nested-dropdown">
+                    <a href="">Old Testament</a>
+                    <div class="nested-dropdown-content">
+
+                        <input type="radio" name="book" value="genesis">Genesis</input><br>
+                        <input type="radio" name="book" value="exodus">Exodus</input><br>
+                        <input type="radio" name="book" value="leviticus">Leviticus</input><br>
+                        <input type="radio" name="book" value="numbers">Numbers</input><br>
+                        <input type="radio" name="book" value="deuteronomy">Deuteronomy</input><br>
+                        <input type="radio" name="book" value="joshua">Joshua</input><br>
+                        <input type="radio" name="book" value="judges">Judges</input><br>
+                        <input type="radio" name="book" value="ruth">Ruth</input><br>
+                        <input type="radio" name="book" value="1samuel">1 Samuel</input><br>
+                        <input type="radio" name="book" value="2samuel">2 Samuel</input><br>
+                        <input type="radio" name="book" value="1kings">1 Kings</input><br>
+                        <input type="radio" name="book" value="2kings">2 Kings</input><br>
+                        <input type="radio" name="book" value="1chronicles">1 Chronicles</input><br>
+                        <input type="radio" name="book" value="2chronicles">2 Chronicles</input><br>
+                        <input type="radio" name="book" value="ezra">Ezra</input><br>
+                        <input type="radio" name="book" value="nehemiah">Nehemiah</input><br>
+                        <input type="radio" name="book" value="esther">Esther</input><br>
+                        <input type="radio" name="book" value="job">Job</input><br>
+                        <input type="radio" name="book" value="psalms">Psalms</input><br>
+                        <input type="radio" name="book" value="proverbs">Proverbs</input><br>
+                        <input type="radio" name="book" value="ecclesiastes">Ecclesiastes</input><br>
+                        <input type="radio" name="book" value="songofsolomon">Song of Songs</input><br>
+                        <input type="radio" name="book" value="isaiah">Isaiah</input><br>
+                        <input type="radio" name="book" value="jeremiah">Jeremiah</input><br>
+                        <input type="radio" name="book" value="lamentations">Lamentations</input><br>
+                        <input type="radio" name="book" value="ezekiel">Ezekiel</input><br>
+                        <input type="radio" name="book" value="daniel">Daniel</input><br>
+                        <input type="radio" name="book" value="hosea">Hosea</input><br>
+                        <input type="radio" name="book" value="joel">Joel</input><br>
+                        <input type="radio" name="book" value="amos">Amos</input><br>
+                        <input type="radio" name="book" value="obadiah">Obadiah</input><br>
+                        <input type="radio" name="book" value="jonah">Jonah</input><br>
+                        <input type="radio" name="book" value="micah">Micah</input><br>
+                        <input type="radio" name="book" value="nahum">Nahum</input><br>
+                        <input type="radio" name="book" value="habakkuk">Habakkuk</input><br>
+                        <input type="radio" name="book" value="zephaniah">Zephaniah</input><br>
+                        <input type="radio" name="book" value="haggai">Haggai</input><br>
+                        <input type="radio" name="book" value="zechariah">Zechariah</input><br>
+                        <input type="radio" name="book" value="malachi">Malachi</input><br>
+                    </div>
+                </div>
+                <div class="nested-dropdown">
+                    <a href="#">New Testament</a>
+                    <div class="nested-dropdown-content">
+                        <input type="radio" name="book" value="matthew">Matthew</input><br>
+                        <input type="radio" name="book" value="mark">Mark</input><br>
+                        <input type="radio" name="book" value="luke">Luke</input><br>
+                        <input type="radio" name="book" value="john">John</input><br>
+                        <input type="radio" name="book" value="acts">Acts</input><br>
+                        <input type="radio" name="book" value="romans">Romans</input><br>
+                        <input type="radio" name="book" value="1corinthians">1 Corinthians</input><br>
+                        <input type="radio" name="book" value="2corinthians">2 Corinthians</input><br>
+                        <input type="radio" name="book" value="galatians">Galatians</input><br>
+                        <input type="radio" name="book" value="ephesians">Ephesians</input><br>
+                        <input type="radio" name="book" value="philippians">Philippians</input><br>
+                        <input type="radio" name="book" value="colossians">Colossians</input><br>
+                        <input type="radio" name="book" value="1thessalonians">1 Thessalonians</input><br>
+                        <input type="radio" name="book" value="2thessalonians">2 Thessalonians</input><br>
+                        <input type="radio" name="book" value="1timothy">1 Timothy</input><br>
+                        <input type="radio" name="book" value="2timothy">2 Timothy</input><br>
+                        <input type="radio" name="book" value="titus">Titus</input><br>
+                        <input type="radio" name="book" value="philemon">Philemon</input><br>
+                        <input type="radio" name="book" value="hebrews">Hebrews</input><br>
+                        <input type="radio" name="book" value="james">James</input><br>
+                        <input type="radio" name="book" value="1peter">1 Peter</input><br>
+                        <input type="radio" name="book" value="2peter">2 Peter</input><br>
+                        <input type="radio" name="book" value="1john">1 John</input><br>
+                        <input type="radio" name="book" value="2john">2 John</input><br>
+                        <input type="radio" name="book" value="3john">3 John</input><br>
+                        <input type="radio" name="book" value="jude">Jude</input><br>
+                        <input type="radio" name="book" value="revelation">Revelation</input><br>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <input type="number" name="chapter" placeholder="Chapter" min="1" max="150" class="ch">
+        <input type="number" name="verse" placeholder="Verse" min="1" max="176" class="vs">
+        <button type="submit" name="submit" class="search">Search</button>
+        <button type="submit" name="random" class="random">Random Verse</button>
+</form>
+
+
+<?php
+
+if(isset($_POST['submit'])){
+
+
+    error_reporting(E_ERROR | E_PARSE);
+    $book=$_POST['book'];
+    $chapter=$_POST['chapter'];
+    $verse=$_POST['verse'];
+    $response = file_get_contents("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/$book/chapters/$chapter/verses/$verse.json");
+    if($response){
+        $data = json_decode($response, true);
+        echo "<p><b>$book $chapter:$verse  </b>";
+        echo $data['text'];
+        echo "</p>";
+    }
+    else {
+        echo "<p>That verse doesn't exist!</p>";
+    }
+    
+}
+
+if(isset($_POST['random'])){
+    error_reporting(E_ERROR | E_PARSE);
+    $books = array("1chronicles","1corinthians","1john","1kings","1peter","1samuel","1thessalonians","1timothy","2chronicles","2corinthians","2john","2kings","2peter","2samuel","2thessalonians","2timothy","3john","acts","amos","colossians","daniel","deuteronomy","ecclesiastes","ephesians","esther","exodus","ezekiel","ezra","galatians","genesis","habakkuk","haggai","hebrews","hosea","isaiah","james","jeremiah","job","joel","john","jonah","joshua","judges","lamentations","leviticus","luke","malachi","mark","matthew","micah","nahum","nehemiah","numbers","obadiah","philemon","philippians","proverbs","psalms","revelation","romans","ruth","songofsolomon","titus","zechariah","zephaniah");
+    $book=array_rand($books,1);
+    do{
+        $chapter=rand(1,150);
+        $verse=1;
+        $response = file_get_contents("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/$books[$book]/chapters/$chapter/verses/$verse.json");
+    }while($response==false);
+    do{
+        $verse=rand(1,176);
+        $response = file_get_contents("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/$books[$book]/chapters/$chapter/verses/$verse.json");
+    }while($response==false);
+    $data = json_decode($response, true);
+    echo "<p><b>$books[$book] $chapter:$verse  </b>";
+    echo $data['text'];
+    echo "</p>";
+}
+?>
+</div>
+</body>
+</html>
